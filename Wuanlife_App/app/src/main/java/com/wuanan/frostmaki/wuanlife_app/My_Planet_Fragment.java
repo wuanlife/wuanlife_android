a8c102b903f858aacc5b44d59f07d5276661362a
“Frostmaki” <stephen.miao1219@gmail.com>
2016-07-14 23:29:25 +0800

    我的星球初步完成

--- Wuanlife_App/app/src/main/java/com/wuanan/frostmaki/wuanlife_app/My_Planet_Fragment.java
+++ Wuanlife_App/app/src/main/java/com/wuanan/frostmaki/wuanlife_app/My_Planet_Fragment.java
@@ -6,11 +6,19 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 public class My_Planet_Fragment extends Fragment{
     private TextView textView;
+    private ListView my_planet_ListView;
+    private ArrayList<String> my_planet_arrayList;
+    private BaseAdapter my_planet_baseAdapter;
 
     @Nullable
     @Override
@@ -18,7 +26,18 @@ public class My_Planet_Fragment extends Fragment{
         View view=inflater.inflate(R.layout.my_planet,container,false);
         textView= (TextView) view.findViewById(R.id.textview1);
         String text=getArguments().getString("text");
-        textView.setText("你好，这里是"+text);
+        //textView.setText("你好，这里是"+text);
+
+        my_planet_ListView= (ListView) view.findViewById(R.id.my_planet_listView);
+        my_planet_arrayList=new ArrayList<String>();
+
+        for (int i=1;i<=20;++i){
+            my_planet_arrayList.add("我的星球：  "+i);
+        }
+        my_planet_baseAdapter=new My_planet_listview_BaseAdapter(getActivity().getApplicationContext(),
+                my_planet_arrayList);
+
+        my_planet_ListView.setAdapter(my_planet_baseAdapter);
         return view;
     }
 }
